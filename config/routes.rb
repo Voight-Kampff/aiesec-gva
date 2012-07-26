024134fec2affb82b9a2b089833d4115c970b0b3
Voight Kampff <valfells@gmail.com>
2012-07-26 22:39:57 +0200

    Demo

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,12 @@
 Aiesec::Application.routes.draw do
+
+  match '/home',    to: 'static_pages#home'
+  match '/about',   to: 'static_pages#about'
+  match '/contact', to: 'static_pages#contact'
+  
+  
+  root to: 'static_pages#home'
+  
   # The priority is based upon order of creation:
   # first created -> highest priority.
 
